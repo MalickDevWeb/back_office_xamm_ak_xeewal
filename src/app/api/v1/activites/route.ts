86a81bdb69f56c00a4e6b4be0e7adfa6a02f34fc
MalickDevWeb <malickteuw.devweb@gmail.com>
2026-08-28 21:03:15 +0000

    ppe exit

--- src/app/api/v1/activites/route.ts
+++ src/app/api/v1/activites/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs';
 import { NextResponse } from 'next/server';
 import { prisma } from '../../../../core/lib/prisma';
 import { withAuth } from '../../../../core/middlewares/authGuard';
